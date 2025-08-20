c4715f1db44e9bd919af94d22d4e4791d60132bf
Kavindu Bandara <it24101943@my.sliit.lk>
2025-08-20 09:33:10 +0530

    initial commit

---
+++ src/main/java/core/Y2S1GroupProjectApplication.java
@@ -0,0 +1,13 @@
+package core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y2S1GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Y2S1GroupProjectApplication.class, args);
+	}
+
+}
